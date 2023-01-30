8914c6244be84c2526a2ba47e40800a27ef9f208
VenkataBhaskarr <venkatabhaskarcodes@gmail.com>
2023-01-30 18:28:38 +0530

    updated user interface

--- frontend/gotoresource/src/App.js
+++ frontend/gotoresource/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Contribute from './components/Contribute';
+import About from './components/About';
+import {Route,Routes} from "react-router-dom"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
-       welcome to the club yogesh,manikanta and bhaskar
+       <ResponsiveAppBar />
+       <Routes>
+         <Route exact path="/" element = {<Home />}> </Route>
+         <Route exact path = "/About.js" element={<About />}> </Route>
+         <Route exact path = "/Contribute.js" element={<Contribute />}> </Route>
+       </Routes>
+
+         {/* <Navbar />
+
+         <Routes> 
+              <Route path="/Contribute.js" element={<Contribute />}></Route>
+              {/* <Route path="https://youtube.com" element={<About />}></Route> */}
+         {/* </Routes> */} 
     </div>
   );
 }
