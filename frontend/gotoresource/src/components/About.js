b7c605747314e8790c3f1b9d7d15188769ccaf45
VenkataBhaskarr <venkatabhaskarcodes@gmail.com>
2023-02-03 06:37:51 +0530

    modified files

--- frontend/gotoresource/src/components/About.js
+++ frontend/gotoresource/src/components/About.js
@@ -1,7 +1,29 @@
+import axios from "axios"
+import MediaCard from "./MediaCard"
+import { useState, useEffect } from "react";
 export default function About(){
+    const [data, setData] = useState(null);
+     useEffect(() => {
+      axios
+        .get('http://localhost:5000/data')
+        .then((res) => {
+          setData(res.data)
+        }).catch(err => console.log(err)) 
+     },[]);
+   if(data){
     return(
         <div>
-            i am about 
+           {
+               data.map((element)=><MediaCard name={element.name} age={element.age}/>)
+           }
         </div>
     )
+   }
+   else{
+    return(
+        <div>
+           this is resource page 
+        </div>
+    )
+   }
 }
--- frontend/gotoresource/src/components/Contribute.js
+++ frontend/gotoresource/src/components/Contribute.js
@@ -1,7 +1,24 @@
+import axios from "axios"
+import {useState} from "react"
 export default function Contribute(){
+    const [name , setName] = useState('')
+    const [age , setAge] = useState('')
+    function sendData(e){
+         e.preventDefault(); 
+         axios.post("http://localhost:5000/data" , {
+            name : name,
+            age : age
+         }).then(res => console.log(res)).catch(err => console.log(err))
+         setName('');
+         setAge('');
+    }
     return(
         <div>
-            contribute to opensource
+           <form>
+                 <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                 <input type="number" value={age} onChange={(e) => setAge(e.target.value)}></input>
+                 <button onClick={sendData}> Submit </button>
+           </form>
         </div>
     )
 }
